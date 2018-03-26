f44c854252f68086b4e8744ce7fc14d34cd3fd7b
gstmariani1025 <gstmariani04@gmail.com>
2018-03-27 00:10:28 +0800

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import BelajarLayout from './src/comp/BelajarLayout';
+import {
+  AppRegistry,
+  StyleSheet,
+  View
+} from 'react-native';
+import Header from './src/Header';
+import Footer from './src/Footer';
+import Weather from './src/Weather';
 
 export default class App extends React.Component {
   render() {
     return (
-      <BelajarLayout />
+      <View style={styles.containerMain}>
+        <Header headerText="Header"/>
+        <Weather/>
+        <Footer footerText="Footer"/>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  containerMain: {
+    paddingTop: 20,
+    flex: 1,
+    backgroundColor: '#E8EAF6'
+  }
+});
